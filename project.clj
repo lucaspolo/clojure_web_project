e9d665910585605ee3b45617c44614f7771a0ec9
Lucas Polo <pradopolo@gmail.com>
2018-10-23 10:43:28 -0300

    Criado e configurado primeiro Handler

--- project.clj
+++ project.clj
@@ -4,4 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                [ring "1.2.0"]])
+                [ring "1.2.0"]]
+  :plugins [[lein-ring "0.8.7"]]
+  :ring {:handler project1.core/example-handler
+         :init    project1.core/on-init
+         :destroy project1.core/on-destroy})
--- src/project1/core.clj
+++ src/project1/core.clj
@@ -4,3 +4,12 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn example-handler [request]
+  {:body "Hello Clojure! Reloaded."})
+
+(defn on-init []
+  (println "Initializing sample webapp!"))
+
+(defn on-destroy []
+  (println "Destroying sample webapp!"))

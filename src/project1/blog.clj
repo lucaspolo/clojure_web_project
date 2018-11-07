4c6e45f7fd4cb8fab60dce857ddbfbac82d51aad
Lucas Polo <pradopolo@gmail.com>
2018-11-07 14:40:18 -0200

    Criado middlware para capturar exceções do blog.

--- src/project1/blog.clj
+++ src/project1/blog.clj
@@ -43,6 +43,15 @@
   (walk/keywordize-keys
     (json/read-str (slurp (:body request)))))
 
+(defn json-error-handler [handler]
+  (fn [request]
+    (try 
+      (handler request)
+      (catch Throwable throwable
+        (assoc (json-response {:message (.getMessage throwable)
+                               :stacktrace (map str (.getStackTrace throwable))})
+            :status 500)))))
+
 (defn get-id [request]
   (Long/parseLong (-> request :route-params :id)))
 
@@ -62,9 +71,11 @@
   (json-response (delete-blog-entry (get-id request))))
 
 (def blog-handler
-  (route/routing
-    (route/with-route-matches :get      "/entries"       get-handler)
-    (route/with-route-matches :post     "/entries"       post-handler)
-    (route/with-route-matches :get      "/entries/:id"   get-entry-handler)
-    (route/with-route-matches :put      "/entries/:id"   put-handler)
-    (route/with-route-matches :delete   "/entries/:id"   delete-handler)))
+  (->
+    (route/routing
+      (route/with-route-matches :get      "/entries"       get-handler)
+      (route/with-route-matches :post     "/entries"       post-handler)
+      (route/with-route-matches :get      "/entries/:id"   get-entry-handler)
+      (route/with-route-matches :put      "/entries/:id"   put-handler)
+      (route/with-route-matches :delete   "/entries/:id"   delete-handler))
+  json-error-handler))

3f1b20a27b7f2f9fc27e31f677f9f5a0928cad79
Lucas Polo <pradopolo@gmail.com>
2018-11-19 16:29:10 -0200

    Inclusao de ClojureScript

--- project.clj
+++ project.clj
@@ -8,7 +8,10 @@
                  [org.postgresql/postgresql "9.2-1003-jdbc4"]
                  [korma "0.3.0-RC6"]
                  [ring "1.2.0"]]
-  :plugins [[lein-ring "0.8.7"]]
+  :plugins [[lein-ring "0.8.7"]
+            [lein-cljsbuild "1.0.3"]]
   :ring {:handler project1.core/full-handler
          :init    project1.core/on-init
-         :destroy project1.core/on-destroy})
+         :destroy project1.core/on-destroy}
+  :cljsbuild {:builds [{:source-paths ["cljs"]
+                        :compiler {:output-to "resources/public/app.js"}}]})
